970fb1fe5613ff77f16ce0764e8cdca77155664e
VonKleistL <luke.edgar@hotmail.com>
2025-11-12 07:20:18 +0100

    Initial Commit

---
+++ OyVey/OyVeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  OyVeyApp.swift
+//  OyVey
+//
+//  Created by Luke Edgar on 12/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct OyVeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
